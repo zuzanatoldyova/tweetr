756cedc05ac72ccfa7eb19f27719b0757a9d5a6b
zuzanatoldyova <zuzana.toldyova@gmail.com>
2017-02-23 03:22:16 +0000

    refactored code

--- server/index.js
+++ server/index.js
@@ -8,6 +8,8 @@ const bodyParser    = require("body-parser");
 const app           = express();
 const path          = require('path');
 const {MongoClient} = require("mongodb");
+const DataHelpers   = require("./lib/data-helpers.js");
+const tweetsRoutes  = require("./routes/tweets");
 const MONGODB_URI   = "mongodb://localhost:27017/tweeter";
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.resolve(__dirname, "../public")));
@@ -21,18 +23,14 @@ MongoClient.connect(MONGODB_URI, (err, db) => {
 //
 // Because it exports a function that expects the `db` as a parameter, we can
 // require it and pass the `db` parameter immediately:
-  const DataHelpers = require("./lib/data-helpers.js")(db);
-  const tweetsRoutes = require("./routes/tweets")(DataHelpers);
-  app.use("/tweets", tweetsRoutes);
-});
 // The `tweets-routes` module works similarly: we pass it the `DataHelpers` object
 // so it can define routes that use it to interact with the data layer.
 
 
 // Mount the tweets routes at the "/tweets" path prefix:
 
-
-
+  app.use("/tweets", tweetsRoutes(DataHelpers(db)));
+});
 
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
